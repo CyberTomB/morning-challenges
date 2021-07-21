3983b6e1b44fa68c1d1b45d01279d6d9514a4c99
CyberDack <afntwrk.guyrpg@gmail.com>
2021-07-21 09:40:40 -0600

    factorize function

--- main.js
+++ main.js
@@ -6,6 +6,25 @@ function isPalindrome(str) {
    return strSpaceless === strRev
 }
 
-function mostCommonLetter(str) {
-
+function factorize(event) {
+   event.preventDefault()
+   let form = event.target
+   let num = form.num.value
+   let str = `The factors of ${num} are `
+   if (num < 1 || num % 1 != 0) {
+      str = "Please enter a positive whole number."
+   }
+   if (num == 1) {
+      str = `1 is a factor of itself.`
+   } else {
+      for (let i = 1; i <= num; i++) {
+         if (num % i == 0 && i != num) {
+            str += `${i}, `
+         }
+      }
+      str += `and ${num}`
+   }
+   console.log(str)
+   document.getElementById('factorize-output').innerText = str
+   form.reset()
 }
